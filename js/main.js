1732b69c4e6c2419074a58a2e85ae886a90c677f
Byt1k <byt1kip82@gmail.com>
2022-06-08 00:51:30 +0300

    added desktop version of the homepage

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$('.doctors__slider').owlCarousel({
+    loop: true,
+    margin: 100,
+    nav: true,
+    dots: false,
+    navText: [
+        '<img src="img/prev.svg" alt="prev"/>',
+        '<img src="img/next.svg" alt="next"/>'
+    ],
+    responsive:{
+        1200:{
+            items: 3
+        }
+    }
+});
+
+$('.news__slider').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: true,
+    navText: [
+        '<img src="img/prev.svg" alt="prev"/>',
+        '<img src="img/next.svg" alt="next"/>'
+    ],
+    responsive:{
+        1200:{
+            items: 3
+        }
+    }
+});
+
+
+const formInputs = document.querySelectorAll('.input input');
+formInputs.forEach(input => {
+    input.addEventListener('click', (e) => {
+        const inputWrapper = e.target.parentNode;
+        inputWrapper.classList.add('active');
+        
+        document.addEventListener('click', (element) => {
+            if (element.target !== inputWrapper 
+                && element.target !== inputWrapper.querySelector('img')
+                && element.target !== inputWrapper.querySelector('input')) {
+                    inputWrapper.classList.remove('active');
+                } 
+        })
+    })
+})
